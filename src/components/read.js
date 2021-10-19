a922e45bfcb415947f92bc46e2296a2a0a0ae8da
Sean King <seank579@gmail.com>
2021-10-19 17:50:27 +0100

    installed axios and completed first 2 tasks

--- src/components/read.js
+++ src/components/read.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
 import Movies from './movies';
+import axios from 'axios';
 
 class Read extends Component {
 
     // Create state array object and movies object to store JSON movie data
     state = {
-        movies: [
-            {
-                "Title": "Avengers: Infinity War",
-                "Year": "2018",
-                "imdbID": "tt4154756",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Captain America: Civil War",
-                "Year": "2016",
-                "imdbID": "tt3498820",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Charlie Wilson's War",
-                "Year": "2007",
-                "imdbID": "tt0472062",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-        ]
+        movies: []
+    };
+
+    //component life cycle hook - promise
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/894944504570986496')
+            //fulfulled state
+            .then(
+                //updates movies array in state with data from json
+                (response) => {
+                    this.setState({ movies: response.data.movies})
+                })
+            //rejected state - error message
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     render() {
